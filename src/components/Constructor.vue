1350a749f802b438590a40f0871b059cb0840287
AlexMiasoiedov <alexerna8@gmail.com>
2017-08-25 17:16:50 +0300

    initial

---
+++ src/components/Constructor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="constructor">
+    <h3>Constructor</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'constructor'
+  }
+</script>
+
+<style scoped>
+
+</style>
